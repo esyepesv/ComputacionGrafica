db38993e27c0f69710255ee916fab2cd578e2cad
esyepesv <60147085+esyepesv@users.noreply.github.com>
2021-03-06 21:02:15 -0500

    transformaciones 3d

---
+++ transformaciones_3d/src/transformaciones_3d/Matrix4x4.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package transformaciones_3d;
+
+/**
+ *
+ * @author estiv
+ */
+public class Matrix4x4 {
+    private double[][] matrix = new double[4][4];
+    
+    public Matrix4x4(double x11, double x12, double x13, double x14,
+                     double x21, double x22, double x23, double x24, 
+                     double x31, double x32, double x33, double x34,
+                     double x41, double x42, double x43, double x44){
+        matrix[0][0] = x11;
+        matrix[0][1] = x12;
+        matrix[0][2] = x13;
+        matrix[0][3] = x14;
+        matrix[1][0] = x21;
+        matrix[1][1] = x22;
+        matrix[1][2] = x23;
+        matrix[1][3] = x24;
+        matrix[2][0] = x31;
+        matrix[2][1] = x32;
+        matrix[2][2] = x33;
+        matrix[2][3] = x34;
+        matrix[3][0] = x41;
+        matrix[3][1] = x42;
+        matrix[3][2] = x43;
+        matrix[3][3] = x44;
+    }
+    
+    public double[][] getMatrix(){
+        return matrix;
+    }
+    
+    public void setMatrix(double[][] matrix){
+        this.matrix = matrix;
+    }
+    
+    public static Point4 times(Matrix4x4 matrix, Point4 p){
+        Point4 r = new Point4(0,0,0,0);
+        double[][] m = matrix.getMatrix();
+        
+        double x = 0;
+        x += m[0][0] * p.getx();
+        x += m[0][1] * p.gety();
+        x += m[0][2] * p.getz();
+        x += m[0][3] * p.getw();
+        r.setx(x);
+        
+        double y = 0;
+        y += m[1][0] * p.getx();
+        y += m[1][1] * p.gety();
+        y += m[1][2] * p.getz();
+        y += m[1][3] * p.getw();
+        r.sety(y);
+        
+        double z = 0;
+        z += m[2][0] * p.getx();
+        z += m[2][1] * p.gety();
+        z += m[2][2] * p.getz();
+        z += m[2][3] * p.getw();
+        r.setz(z);
+        
+        double w = 0;
+        w += m[3][0] * p.getx();
+        w += m[3][1] * p.gety();
+        w += m[3][2] * p.getz();
+        w += m[3][3] * p.getw();
+        r.setw(w);
+            
+        return r;
+    }
+    
+    public static Matrix4x4 times(Matrix4x4 m1, Matrix4x4 m2){
+        Matrix4x4 m3 = new Matrix4x4(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        double[][] m = m3.getMatrix();
+        for(int i=0; i<4; i++){
+            for(int j=0; j<4; j++){
+                for(int k=0; k<4; k++){
+                    m[i][j] += m1.getMatrix()[i][k] * m2.getMatrix()[k][j];
+                }
+            }
+        }
+        m3.setMatrix(m);
+        return m3;
+    }
+}
--- transformaciones_3d/src/transformaciones_3d/Transformaciones_3d.java
+++ transformaciones_3d/src/transformaciones_3d/Transformaciones_3d.java
@@ -9,30 +9,224 @@ package transformaciones_3d;
  *
  * @author estiv
  */
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.geom.Line2D;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import javax.swing.JPanel;
 import javax.swing.JFrame;
-public class Transformaciones_3d extends JPanel{
+
+public final class Transformaciones_3d extends JPanel
+    implements KeyListener {
     
     Scanner in;
     Scanner line;
-    Edge[] edges;
-    int[][] puntos;
+    int[][] vertices;
+    double[][] puntos;
+    Line2D.Double linea1;
     
+    public Transformaciones_3d() {
+        linea1 = new Line2D.Double();
+        this.addKeyListener(this);
+        leerArchivo();
+    }
      @Override
   public void paintComponent(Graphics g) {
-      //super.paintComponent(g);
+      super.paintComponent(g);
 
       Graphics2D g2d = (Graphics2D) g;
       g2d.setColor(Color.blue);
+      dibujar(g2d);
+  }
+  
+  public void dibujar(Graphics2D g2d){
+        double d = -1000;
+        for (int[] vertice : vertices) {
+            int inicio = vertice[0];
+            int fin = vertice[1];
+            
+            double[] edge1 = puntos[inicio];
+            double d1 = edge1[2]/d;
+            
+            double[] edge2 = puntos[fin];
+            double d2 = edge2[2]/d;
+            
+            linea1.x1 = Math.round(edge1[0]/d1);
+            linea1.y1 = Math.round(edge1[1]/d1);
+            linea1.x2 = Math.round(edge2[0]/d2);
+            linea1.y2 = Math.round(edge2[1]/d2);
+            
+            g2d.draw(linea1);
+        }
+  }
+  
+  public void nuevosPuntos(Matrix4x4 m){
+        for (double[] punto : puntos) {
+            Point4 p = new Point4(punto[0], punto[1], punto[2], 1);
+            p = Matrix4x4.times(m,p);
+            punto[0] = p.getx();
+            punto[1] = p.gety();
+            punto[2] = p.getz();
+        }
+  }
+  
+  public void traslacion(double dx, double dy, double dz){
+      Matrix4x4 m = new Matrix4x4(1, 0, 0, dx,
+                                  0, 1, 0, dy,
+                                  0, 0, 1, dz,
+                                  0, 0, 0, 1 );
+      nuevosPuntos(m);
+  }
+  
+  public void rotacionz(double angulo){
+      Matrix4x4 m = new Matrix4x4(Math.cos(angulo), -Math.sin(angulo), 0, 0,
+                                  Math.sin(angulo), Math.cos(angulo), 0, 0,
+                                  0, 0, 1, 0,
+                                  0, 0, 0, 1);
       
+      double xi = promedio(0);
+      double yi = promedio(1);
+      double zi = promedio(2);      
+      nuevosPuntos(m);      
+      double xf = promedio(0);
+      double yf = promedio(1);
+      double zf = promedio(2);
+      //traslacion inversa
+      traslacion(xi-xf, yi-yf, zi-zf);
+  }
+  
+  public void rotacionx(double angulo){
+      Matrix4x4 m = new Matrix4x4(1, 0, 0, 0,
+                                  0, Math.cos(angulo), -(Math.sin(angulo)), 0,
+                                  0, Math.sin(angulo), Math.cos(angulo), 0,
+                                  0, 0, 0, 1);
+      
+      double xi = promedio(0);
+      double yi = promedio(1);
+      double zi = promedio(2);      
+      nuevosPuntos(m);      
+      double xf = promedio(0);
+      double yf = promedio(1);
+      double zf = promedio(2);
+      //traslacion inversa
+      traslacion(xi-xf, yi-yf, zi-zf);
+  }
+  
+  public void rotaciony(double angulo){
+      Matrix4x4 m = new Matrix4x4(Math.cos(angulo), 0, Math.sin(angulo), 0,
+                                  0, 1, 0, 0,
+                                  -Math.sin(angulo), 0, Math.cos(angulo), 0,
+                                  0, 0, 0, 1);
+      
+      double xi = promedio(0);
+      double yi = promedio(1);
+      double zi = promedio(2);      
+      nuevosPuntos(m);      
+      double xf = promedio(0);
+      double yf = promedio(1);
+      double zf = promedio(2);
+      //traslacion inversa
+      traslacion(xi-xf, yi-yf, zi-zf);
+  }
+  
+  public void escalamiento(double sx, double sy, double sz){
+      Matrix4x4 m = new Matrix4x4(sx, 0, 0, 0,
+                                  0, sy, 0, 0,
+                                  0, 0, sz, 0,
+                                  0, 0, 0, 1);
+      
+      double xi = promedio(0);
+      double yi = promedio(1);
+      double zi = promedio(2);      
+      nuevosPuntos(m);      
+      double xf = promedio(0);
+      double yf = promedio(1);
+      double zf = promedio(2);
+      //traslacion inversa
+      traslacion(xi-xf, yi-yf, zi-zf);
+  }
+  
+  // promedio de todos los puntos
+  public double promedio(int x){
+      double res = 0;
+      for(double[] punto : puntos){
+          res+=punto[x];
+      }
+      res/=puntos.length;
+      return res;
+  }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+       switch (e.getKeyChar()){
+        case 'a' -> {
+            traslacion(-10, 0, 0);
+            repaint();
+            }
+        case 'd' -> {
+            traslacion(10, 0, 0);
+            repaint();
+            }
+        case 'w' -> {
+            traslacion(0, -10, 0);
+            repaint();
+            }
+        case 's' -> {
+            traslacion(0, 10, 0);
+            repaint();
+            }
+        case 'q' -> {
+            rotacionz(25);
+            repaint();
+            }
+        case 'e' -> {
+            rotacionz(-25);
+            repaint();
+            }
+        case 'r' -> {
+            rotacionx(25);
+            repaint();
+            }
+        case 't' -> {
+            rotacionx(-25);
+            repaint();
+            }
+        case 'y' -> {
+            rotaciony(25);
+            repaint();
+            }
+        case 'u' -> {
+            rotaciony(-25);
+            repaint();
+            }
+        case 'i' -> {
+            escalamiento(1.1 ,1.1, 1.1 );
+            repaint();
+            }
+        case 'k' -> {
+            escalamiento(0.9 , 0.9, 0.9);
+            repaint();
+            }
+        default -> repaint();  
+       }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    
+  public void leerArchivo(){
       String nombre = "C:\\Users\\estiv\\OneDrive\\Documentos\\NetBeansProjects\\transformaciones_3d\\src\\transformaciones_3d\\casita3D.txt";
     
       try {
@@ -42,7 +236,8 @@ public class Transformaciones_3d extends JPanel{
             String lineString = in.nextLine();
             line = new Scanner(lineString );
             int numPuntos = line.nextInt();
-            edges = new Edge[numPuntos];
+            puntos = new double[numPuntos][3];
+            
             
             // Leer los puntos
             for(int punto = 0; punto < numPuntos; punto++) {
@@ -51,55 +246,30 @@ public class Transformaciones_3d extends JPanel{
                 double x = line.nextDouble();
                 double y = line.nextDouble();
                 double z = line.nextDouble();
-                edges[punto] = new Edge(x + 250,250 - y, z);
+                puntos [punto][0]= x + 250;
+                puntos [punto][1] = 250 - y;
+                puntos [punto][2] = z;
             }
             
             // Leer el número de puntos
             lineString = in.nextLine();
             line = new Scanner(lineString );
             int numVertices = line.nextInt();
-            puntos = new int[numVertices][2];
+            vertices = new int[numVertices][2];
             
             // Leer los puntos
             for(int vertice = 0; vertice < numVertices; vertice++) {
                 lineString = in.nextLine();
                 line = new Scanner(lineString);
-                puntos[vertice][0] = line.nextInt();
-                puntos[vertice][1] = line.nextInt();
+                vertices[vertice][0] = line.nextInt();
+                vertices[vertice][1] = line.nextInt();
                
             }
         } catch (FileNotFoundException ex) {
             System.out.println("El archivo " + nombre + " no se encuentra." + ex);
         } 
-      
-      dibujarCasita(g2d);
-      
-  }
   
-  public void dibujarCasita(Graphics2D g2d){
-      
-        int d = -1000;
-        
-        for (int[] punto : puntos) {
-            int inicio = punto[0];
-            int fin = punto[1];
-            Edge edge1 = edges[inicio];
-            double d1 = edge1.getz()/d;
-            Edge edge2 = edges[fin];
-            double d2 = edge2.getz()/d;
-            
-            System.out.println(d1+" "+d2);
-            
-            double x1 = Math.round(edge1.getPoint1()/d1);
-            double y1 = Math.round(edge1.getPoint2()/d1);
-            double x2 = Math.round(edge2.getPoint1()/d2);
-            double y2 = Math.round(edge2.getPoint2()/d2);
-            
-            System.out.println(x1+" "+y1+" "+x2+" "+y2);
-            
-            g2d.drawLine((int) x1,(int) y1,(int) x2,(int) y2);
-        }
-  } 
+  }
  
   public static void main(String[] args) {
       // Crear un nuevo Frame
@@ -107,14 +277,15 @@ public class Transformaciones_3d extends JPanel{
       // Al cerrar el frame, termina la ejecución de este programa
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       // Agregar un JPanel que se llama Points (esta clase)
-      frame.add(new Transformaciones_3d());
+      Transformaciones_3d t = new Transformaciones_3d();
+      frame.add(t);
+      frame.addKeyListener(t);
       // Asignarle tamaño
       frame.setSize(500, 500);
       // Poner el frame en el centro de la pantalla
       frame.setLocationRelativeTo(null);
       // Mostrar el frame
       frame.setVisible(true);
-  }
-    
+  }   
     
 }
